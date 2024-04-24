d1074a38b154cbea3e78e95f948fc63db1422e24
Lenar Mirzayanov <melkiy-dude01@yandex.ru>
2024-04-24 10:16:19 +0300

    upd 5

--- src/components/about/AboutMeBio.jsx
+++ src/components/about/AboutMeBio.jsx
@@ -8,11 +8,12 @@ const AboutMeBio = () => {
     return (
         <div className="block sm:flex sm:gap-10 mt-10 sm:mt-20">
             <div className="w-full sm:w-1/4 mb-7 sm:mb-0">
-                <img  src={profileImage} className="rounded-lg w-96" alt=""/>
+                <img src={profileImage} className="rounded-lg w-96" alt=""/>
             </div>
 
             <div className="font-general-regular w-full sm:w-3/4 text-left">
                 {aboutMe.map((bio) => (
+
                     <>
                         <h3 className="mb-4 font-general-medium text-lg md:text-xl text-ternary-dark dark:text-ternary-light"
                         >
@@ -20,11 +21,20 @@ const AboutMeBio = () => {
                         </h3>
 
                         <p
-                            className="mb-4 text-ternary-dark dark:text-ternary-light text-lg"
-
+                            className="mb-6 text-ternary-dark dark:text-ternary-light text-lg"
                         >
                             {bio.bio}
                         </p>
+
+                        <ul className="mb-4 text-ternary-dark dark:text-ternary-light text-lg">
+                            {bio.skills.map(skill => (
+                                <>
+                                    <li className="text-ternary-dark dark:text-ternary-light text-lg">
+                                        &#8212; {skill}
+                                    </li>
+                                </>
+                            ))}
+                        </ul>
                     </>
                 ))}
             </div>
